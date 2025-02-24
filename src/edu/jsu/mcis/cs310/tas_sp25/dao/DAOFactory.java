455af670185e2ac219dc9c422dad1c423dcf6b5b
MAHIN PATEL <135858914+mahin2406@users.noreply.github.com>
2025-02-24 12:55:52 -0600

    Added Department.java and also added DepartmentDAO in DAOFactory

--- src/edu/jsu/mcis/cs310/tas_sp25/dao/DAOFactory.java
+++ src/edu/jsu/mcis/cs310/tas_sp25/dao/DAOFactory.java
@@ -45,6 +45,9 @@ public final class DAOFactory {
     public EmployeeDAO getEmployeeDAO() {
         return new EmployeeDAO(this);
     }
-
+    
+    public DepartmentDAO getDepartmentDAO(){
+        return new DepartmentDAO(this);
+    }
 
 }
