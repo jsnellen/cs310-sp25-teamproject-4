3b0dc2e5aa8409c42fefc2444f8b651bd4bf6a6d
MAHIN PATEL <135858914+mahin2406@users.noreply.github.com>
2025-03-03 14:47:31 -0600

    Finalize swift.java and swiftDAO.java, This code works smooth and better than last time.

--- src/edu/jsu/mcis/cs310/tas_sp25/Shift.java
+++ src/edu/jsu/mcis/cs310/tas_sp25/Shift.java
@@ -1,9 +1,6 @@
 package edu.jsu.mcis.cs310.tas_sp25;
 
-
-import static java.lang.Math.abs;
 import java.time.LocalTime;
-import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 import java.time.Duration;
 
@@ -13,7 +10,7 @@ import java.time.Duration;
  * @author mahin
  */
 public class Shift {
-    
+
     private int shift_id;
     private int round_Interval;
     private int grace_Period;
@@ -24,31 +21,30 @@ public class Shift {
     private LocalTime shift_Stop;
     private LocalTime lunch_Start;
     private LocalTime lunch_Stop;
-    
+
     public Shift(HashMap<String, Object> theMap) {
-        
-        shift_id = (Integer)theMap.get("shiftid");
-        round_Interval = (Integer)theMap.get("roundinterval");
-        grace_Period = (Integer)theMap.get("graceperiod");
-        dock_Penalty = (Integer)theMap.get("dockpenalty");
-        lunch_Threshold = (Integer)theMap.get("lunchthreshold");    
-        description = (String)theMap.get("description");     
-        lunch_Start = (LocalTime)theMap.get("lunchstart");
-        lunch_Stop = (LocalTime)theMap.get("lunchstop");    
-        shift_Start = (LocalTime)theMap.get("shiftstart");
-        shift_Stop = (LocalTime)theMap.get("shiftstop");
-        
+
+        shift_id = (Integer) theMap.get("shiftid");
+        round_Interval = (Integer) theMap.get("roundinterval");
+        grace_Period = (Integer) theMap.get("graceperiod");
+        dock_Penalty = (Integer) theMap.get("dockpenalty");
+        lunch_Threshold = (Integer) theMap.get("lunchthreshold");
+        description = (String) theMap.get("description");
+        lunch_Start = (LocalTime) theMap.get("lunchstart");
+        lunch_Stop = (LocalTime) theMap.get("lunchstop");
+        shift_Start = (LocalTime) theMap.get("shiftstart");
+        shift_Stop = (LocalTime) theMap.get("shiftstop");
+
     }
-    
 
     public int getShiftid() {
         return shift_id;
     }
-    
+
     public int getRoundInterval() {
         return round_Interval;
     }
-    
+
     public int getGracePeriod() {
         return grace_Period;
     }
@@ -56,7 +52,7 @@ public class Shift {
     public int getDockPenalty() {
         return dock_Penalty;
     }
-    
+
     public int getLunchThreshold() {
         return lunch_Threshold;
     }
@@ -65,54 +61,74 @@ public class Shift {
         return description;
     }
 
-    public LocalTime getShiftstart() {
-        return shift_Start;
-    }
-
-    public LocalTime getShiftstop() {
-        return shift_Stop;
-    }
-
-    public LocalTime getLunchstart() {
+    public LocalTime getLunchStart() {
         return lunch_Start;
     }
 
-    public LocalTime getLunchstop() {
+    public LocalTime getLunchStop() {
         return lunch_Stop;
     }
 
-    
-    
+    public LocalTime getShiftStart() {
+        return shift_Start;
+    }
+
+    public LocalTime getShiftStop() {
+        return shift_Stop;
+    }
+
     public int getShiftDuration() {
-        
-        long minutesInDay = Duration.ofDays(1).toMinutes();
         if (shift_Start.isBefore(shift_Stop)) {
-            return (int)(ChronoUnit.MINUTES.between(shift_Start, shift_Stop));
+            return (int) Duration.between(shift_Start, shift_Stop).toMinutes();
+        } else {
+            return (int) (Duration.between(shift_Start, LocalTime.MAX).toMinutes()
+                    + Duration.between(LocalTime.MIN, shift_Stop).toMinutes());
         }
-        else {
-            return (int)(minutesInDay - ChronoUnit.MINUTES.between(shift_Stop, shift_Start));
-        }
-        
     }
-    
+
     public int getLunchDuration() {
-        
-        return abs((int)(ChronoUnit.MINUTES.between(lunch_Start, lunch_Stop)));
-    
+        return (int) Duration.between(lunch_Start, lunch_Stop).toMinutes();
     }
-  
-    
+
+    /**
+     * Returns a string representation of the Shift object.
+     *
+     * @return A string representation of the shift.
+     */
     @Override
     public String toString() {
-        StringBuilder hello = new StringBuilder();
-        hello.append(this.getDescription()).append(": ").append(this.getShiftstart())
-        .append(" - ").append(this.getShiftstop()).append(" (").append(this.getShiftDuration())
-        .append(" minutes); Lunch: ").append(this.getLunchstart()).append(" - ")
-        .append(this.getLunchstop()).append(" (").append(this.getLunchDuration()).append(" minutes)");
-        
-        String totalStr = hello.toString();
-        
-        return totalStr;
+        return String.format("%s: %s - %s (%d minutes); Lunch: %s - %s (%d minutes)",
+                description,
+                shift_Start, shift_Stop, getShiftDuration(),
+                lunch_Start, lunch_Stop, getLunchDuration());
     }
-    
+
+    /**
+     * Compares this Shift object with another based on the shift ID.
+     *
+     * @param ob The object to compare with.
+     * @return True if the objects are equal, false otherwise.
+     */
+    @Override
+    public boolean equals(Object ob) {
+        if (this == ob) {
+            return true;
+        }
+        if (ob == null || getClass() != ob.getClass()) {
+            return false;
+        }
+        Shift shift = (Shift) ob;
+        return shift_id == shift.shift_id;
+    }
+
+    /**
+     * Generates a hash code for the Shift object based on the shift ID.
+     *
+     * @return A hash code for the Shift object.
+     */
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(shift_id);
+    }
 }
+
--- src/edu/jsu/mcis/cs310/tas_sp25/dao/ShiftDAO.java
+++ src/edu/jsu/mcis/cs310/tas_sp25/dao/ShiftDAO.java
@@ -11,117 +11,97 @@ import java.util.*;
  */
 public class ShiftDAO {
 
-    private static final String QUERY_FIND = "SELECT * FROM shift WHERE id = ?";
-    private static final String QUERY_BADGE = "SELECT * FROM employee WHERE badgeid =?";
+    private static final String QUERY_FIND_SHIFT = "SELECT * FROM shift WHERE id = ?";
+    private static final String QUERY_FIND_EMPLOYEE = "SELECT * FROM employee WHERE badgeid =?";
 
     private final DAOFactory daoFactory;
-
+    
+    /**
+     * Constructor to initialize DAOFactory.
+     * 
+     * @param daoFactory The DAOFactory used to get database connections.
+     */
     public ShiftDAO(DAOFactory daoFactory) {
         this.daoFactory = daoFactory;
     }
 
-    public Shift find(int id) {
+    /**
+     * Finds a Shift by its ID.
+     * @param shiftId The ID of the shift to retrieve.
+     * @return The Shift object associated with the given shift ID, or null if not found.
+     */
+    public Shift find(int shiftId) {
         Shift shift = null;
-
-        PreparedStatement ps = null;
-        ResultSet rs = null;
+        Connection cont = null; // Declaring connection here
 
         try {
-            Connection conn = daoFactory.getConnection();
+            cont = daoFactory.getConnection(); // Get connection
+            PreparedStatement preparedStatement = cont.prepareStatement(QUERY_FIND_SHIFT);
+            preparedStatement.setInt(1, shiftId);
 
-            if (conn.isValid(0)) {
-
-                ps = conn.prepareStatement(QUERY_FIND);
-                ps.setInt(1, id);
-
-                boolean hasresults = ps.execute();
-
-                if (hasresults) {
-                    rs = ps.getResultSet();
-
-                    while (rs.next()) {
-
-                        HashMap<String, Object> hashmap = new HashMap<>();
-                        hashmap.put("shiftid", rs.getInt("id"));
-                        hashmap.put("description", rs.getString("description"));
-                        hashmap.put("shiftstart", LocalTime.parse(rs.getString("shiftstart")));
-                        hashmap.put("shiftstop", LocalTime.parse(rs.getString("shiftstop")));
-                        hashmap.put("roundinterval", rs.getInt("roundinterval"));
-                        hashmap.put("graceperiod", rs.getInt("graceperiod"));
-                        hashmap.put("dockpenalty", rs.getInt("dockpenalty"));
-                        hashmap.put("lunchstart", LocalTime.parse(rs.getString("lunchstart")));
-                        hashmap.put("lunchstop", LocalTime.parse(rs.getString("lunchstop")));
-                        hashmap.put("lunchthreshold", rs.getInt("lunchthreshold"));
-                        shift = new Shift(hashmap);
-
-                    }
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    shift = mapResultSetToShift(resultSet);
                 }
             }
         } catch (SQLException e) {
             throw new DAOException(e.getMessage());
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-                    throw new DAOException(e.getMessage());
-                }
-            }
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                    throw new DAOException(e.getMessage());
-                }
-            }
         }
+
         return shift;
     }
 
+    /**
+     * Finds a Shift based on the Badge ID.
+     * 
+     * @param badge The Badge object containing the badge ID.
+     * @return The Shift object associated with the given badge ID, or null if not found.
+     */
     public Shift find(Badge badge) {
         Shift shift = null;
-
-        PreparedStatement ps = null;
-        ResultSet rs = null;
+        Connection cont = null; // Declaring connection here
 
         try {
+            cont = daoFactory.getConnection(); // Get connection
+            PreparedStatement preparedStatement = cont.prepareStatement(QUERY_FIND_EMPLOYEE);
+            preparedStatement.setString(1, badge.getId());
 
-            Connection conn = daoFactory.getConnection();
-
-            if (conn.isValid(0)) {
-
-                ps = conn.prepareStatement(QUERY_BADGE);
-                ps.setString(1, badge.getId());
-
-                boolean hasresults = ps.execute();
-
-                if (hasresults) {
-
-                    rs = ps.getResultSet();
-                    rs.next();
-                    int shiftid = rs.getInt("shiftid");
-                    shift = this.find(shiftid);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    int shiftId = resultSet.getInt("shiftid");
+                    shift = find(shiftId); //find the shift with the shift ID
                 }
             }
         } catch (SQLException e) {
             throw new DAOException(e.getMessage());
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-                    throw new DAOException(e.getMessage());
-                }
-            }
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                    throw new DAOException(e.getMessage());
-                }
-            }
-        }
+        } 
+
         return shift;
     }
 
+    /**
+     * 
+     * 
+     * @param resultSet The ResultSet containing the shift data.
+     * @return A Shift object populated with the data from the ResultSet.
+     * @throws SQLException If an error occurs while reading from the ResultSet.
+     */
+    private Shift mapResultSetToShift(ResultSet resultSet) throws SQLException {
+        HashMap<String, Object> shiftData = new HashMap<>();
+
+        shiftData.put("shiftid", resultSet.getInt("id"));
+        shiftData.put("description", resultSet.getString("description"));
+        shiftData.put("shiftstart", resultSet.getObject("shiftstart", LocalTime.class));
+        shiftData.put("shiftstop", resultSet.getObject("shiftstop", LocalTime.class));
+        shiftData.put("roundinterval", resultSet.getInt("roundinterval"));
+        shiftData.put("graceperiod", resultSet.getInt("graceperiod"));
+        shiftData.put("dockpenalty", resultSet.getInt("dockpenalty"));
+        shiftData.put("lunchstart", resultSet.getObject("lunchstart", LocalTime.class));
+        shiftData.put("lunchstop", resultSet.getObject("lunchstop", LocalTime.class));
+        shiftData.put("lunchthreshold", resultSet.getInt("lunchthreshold"));
+
+        return new Shift(shiftData);
+        
+    }
+    
 }
